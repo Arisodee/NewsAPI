83c42f533868d3b773dda5c178c976dbc91e13d3
Arisodee <arisodee@gmail.com>
2020-11-30 13:59:12 +0300

    Add functions to get headlines and categories from json

--- app/views.py
+++ app/views.py
@@ -1,13 +1,36 @@
-from flask import render_template
 from app import app
-from .request import get_source
+from flask import render_template,request,redirect,url_for
+from .request import get_source,article_source,get_category,get_headlines
+
 
 # Views
+
 @app.route('/')
 def index():
-
     '''
-    View root page function that returns the index page and its data
+    Root function returning index/home page with data
     '''
     source= get_source()
-    return render_template('index.html',sources=source)
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
+
+@app.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id= id )
+
+@app.route('/categories/<cat_name>')
+def category(category):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(category)
+    title = f'{category}'
+    category = category
+
+    return render_template('categories.html',title = title,category = category)
