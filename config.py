58d3c52000053fea69e19cf01a91221a4f1ead4b
Arisodee <arisodee@gmail.com>
2020-12-02 00:29:24 +0300

    Add secret key

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
     ARTICLES_BASE_URL='https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY= os.environ.get(SECRET_KEY)
 
 
 class ProdConfig(Config):
