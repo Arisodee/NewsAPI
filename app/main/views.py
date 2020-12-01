6752e7583719ac5485cf77de9e3cc91e710e68a7
Arisodee <arisodee@gmail.com>
2020-12-01 19:34:52 +0300

    Add styling, Change Application structure and fix some errors

--- app/main/views.py
+++ app/main/views.py
@@ -7,27 +7,20 @@ from ..models import Source
 @main.route('/')
 def index():
 	'''
-	view root page function that returns index page and its data
+	Root function that returns index page and its data
 	'''
-
-	entertainment_sources = get_sources('entertainment')
-	health_sources = get_sources('health')
 	general_sources = get_sources('general')
-	science_sources = get_sources('science')
-	sports_sources = get_sources('sports')
-	technology_sources = get_sources('technology')
-	business_sources = get_sources('business')
 	title = "News"
-	 
 
-	return render_template('index.html', entertainment=entertainment_sources, health=health_sources, general=general_sources, science=science_sources, sports=sports_sources, technology=technology_sources, business=business_sources)
+	return render_template('index.html', title=title, general=general_sources)
+		
 
-@main.route('/sources/<id>')
+@main.route('/articles/<id>')
 def articles(id):
 	'''
 	view articles page
 	'''
 	articles = get_articles(id)
-	title = f'NH | {id}'
+	title = f'{id}'
 
 	return render_template('articles.html',title=title, articles=articles) 
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    ARTICLES_BASE_URL='https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+    ARTICLES_BASE_URL='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
 
